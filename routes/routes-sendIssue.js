e5147f88ced272b0fc3b8bce56317eccd907bbdb
Adis Hodzic <ahodzic9@etf.unsa.ba>
2019-05-14 17:22:06 +0200

    Prepravka baze na sequelize

--- routes/routes-sendIssue.js
+++ routes/routes-sendIssue.js
@@ -1,27 +1,24 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../db.js')
+module.exports = (app, db) => {
 
-router.post('/issue/send', function(req, res){
-
-    if(req.body.issueText.length == 0) res.send("Upišite sadržaj poruke");
-
-    //Prvo pronaći id kategorije u tabeli Category
-    db.query("SELECT id FROM Category WHERE DisplayName = '" + req.body.issueTitle + "';", function (err, result, fields){
-        if(err) throw err;
-        id = parseInt(result[result.length-1]['id']);
-        id.toString();
-
-        //Sada kad smo nasli id kategorije, trazimo id studenta
-        //Međutim, pošto još uvijek nemamo login gotov, hardkodirat ćemo id studenta
-
-        db.query("INSERT INTO Issue (idStudent, idCategory, issueStatus, message) VALUES (1, " + id + ", 'new', '" + req.body.issueText + "');", function (err, result, fields) {
-            if (err) throw err;
-            res.send("Uspjesno poslan issue!");
+    app.post('/issue/send',function(req, res){
+        db.issueCategory.findOne({where:{naziv:req.query.issueTitle}}).then(kategorija => {
+            //console.log(kategorija);
+            const noviIssue = db.issue.build({
+                status: "new",
+                procitaoStudent: false,
+                procitalaSS: false,
+                issueID: kategorija.id,
+                studentID: "2"
+            }).save().then(x => {
+                console.log("usao");
+                console.log(x.id);
+                console.log(new Date());
+                const noviMessage = db.issueMessage.build({
+                tekst: req.body.issueText,
+                //datum: new Date(),
+                messageID: x.id
+            }).save().then(x => {res.send("Uspjesan upis!")});    
+        }).catch(error => res.send(error))});
         });
-    })
-
-});  		
-
-module.exports = router; 
+    }
 
