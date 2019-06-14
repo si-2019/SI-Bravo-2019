05ed44da27da5ce5cb94c84bfb4e6f2f14c7f973
Adis Hodzic <ahodzic9@etf.unsa.ba>
2019-06-14 19:40:29 +0200

    Dodan backend za odabir srudenta od SS

--- routes/routes-sendIssue.js
+++ routes/routes-sendIssue.js
@@ -23,23 +23,30 @@ module.exports = (app, db) => {
     });
 
     app.post('/issue/send/ss',function(req, res){
-        db.issueCategory.findOne({where:{naziv:req.query.issueTitle}}).then(kategorija => {
-            const noviIssue = db.issue.build({
-                status: "new",
-                procitaoStudent: false,
-                procitalaSS: true,
-                categoryID: kategorija.id,
-                StudentID: 1,
-                draftStatus: 0,
-                trashStudent: 0,
-                trashSS: 0
-            }).save().then(x => {
-                const noviMessage = db.issueMessage.build({
-                tekst: req.query.issueText,
-                datum: new Date(),
-                issueID: x.id,
-                draftStatus: 0
-            }).save().then(x => {res.send("Uspjesan upis!")});    
-        }).catch(error => res.send(error))});
+        db.korisnik.findOne({where:{username:req.query.username}}).then(student => {
+            if(student != null)
+            {
+            db.issueCategory.findOne({where:{naziv:req.query.issueTitle}}).then(kategorija => {
+                const noviIssue = db.issue.build({
+                    status: "new",
+                    procitaoStudent: false,
+                    procitalaSS: true,
+                    categoryID: kategorija.id,
+                    StudentID: 1,
+                    draftStatus: 0,
+                    trashStudent: 0,
+                    trashSS: 0
+                }).save().then(x => {
+                    const noviMessage = db.issueMessage.build({
+                    tekst: req.query.issueText,
+                    datum: new Date(),
+                    issueID: x.id,
+                    draftStatus: 0
+                }).save().then(x => {res.send("Uspjesan upis!")});    
+            }).catch(error => res.send(error))});
+            }
+            else
+                res.send("Korisnik ne postoji!");
+        }).catch(error => res.send(error));
     });
 }
