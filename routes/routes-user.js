63544d899c0a542c965dd318b33d83ee50a17f49
Adis Hodzic <ahodzic9@etf.unsa.ba>
2019-06-15 16:41:42 +0200

    Ispravljeno dobavljanje studenata i dodan unit test

--- routes/routes-user.js
+++ routes/routes-user.js
@@ -1,6 +1,6 @@
 module.exports = (app, db) => {
     app.get('/korisnici/get', function(req, res) {
-        db.korisnik.findAll().then(rezultat => {
+        db.korisnik.findAll({where:{idUloga:1}}).then(rezultat => {
             res.send(rezultat);             
         }).catch(error => res.send("Greška prilikom čitanja iz baze!"));
     });
